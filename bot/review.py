8fa2f3730a47a8737443f3fb6cae0673f7399484
Julius Parkin <julius.parkin@icloud.com>
2026-04-28 19:31:54 -0400

    LLM integration using claudes API

--- bot/review.py
+++ bot/review.py
@@ -1,2 +1,23 @@
+import anthropic
+import os
+
+client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+
+diff = """
++ def add(a, b):
++   return a - b
+
+"""
+
+response = client.messages.create(
+    model = "claude-opus-4-5",
+    max_tokens = 1024,
+    messages = [
+        {"role": "user", "content": f"Review this code diff and flag any bugs or issues:\n\n{diff}"}
+    ]
+
+)
+
 print("PRism bot has been triggered")
 print("Pull request has been opened or updated")
+print(response.content[0].text)
